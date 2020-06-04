b5a77f9abae6d46845402241e0c4c7e8b58c739d
Aakriti <aakriti_jain22@yahoo.in>
2020-06-04 21:41:04 +0530

    First Commit message

---
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void AndriodApp()
+	{
+		System.out.println("Method=AndriodApp");
+	}
+	
+	@Test
+	public void iosApps()
+	{
+		System.out.println("iosApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("Method=browserAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+
+}
